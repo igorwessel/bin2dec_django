7a6dc624828707a521999f629931ed0baac15df9
Igor Wessel da Silva <igor_wessel@hotmail.com>
2020-02-19 21:28:38 -0300

    implemented user notify when typing diferrent 0 or 1

--- templates/static/index.js
+++ templates/static/index.js
@@ -2,17 +2,35 @@ let div_container = document.querySelector('.container')
 let binary_number = document.getElementById('binary_input')
 
 binary_number.addEventListener('input', (event) => {
-    console.log(binary_number.value)
-    msg_error_element = msg_error()
+    msg_error(binary_number.value)
 })
 
 
-function msg_error() {
+function msg_error(input_value) {
     let msg_error = document.createElement('p')
     let msg_text_error = document.createTextNode('Favor insira 0 ou 1.')
     msg_error.className = 'msg-error'
     msg_error.id = 'msg-error'
     msg_error.appendChild(msg_text_error)
-    return msg_error
+    msg_error_element = document.querySelector('.msg-error')
+
+    have_0_or_1(input_value, msg_error)
+
+    if (input_value == '') {
+        if (msg_error_element != null) {
+            msg_error_element.remove()
+        }
+    }
+}
+
+
+function have_0_or_1(string, new_element) {
+    for (letter = 0; letter < string.length; letter++) {
+        if (string[letter] != 0 && string[letter] != 1) {
+            if (msg_error_element == null) {
+                binary_number.insertAdjacentElement('beforebegin', new_element)
+            }
+        }
+    }
 }
 
